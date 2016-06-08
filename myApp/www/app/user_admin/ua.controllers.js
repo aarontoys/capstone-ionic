cef44911bc006ca87a0e86273ed9bb2ab51a9c79
Aaron Toys <aarontoys@gmail.com>
2016-06-08 01:02:01 -0600

    date picker now adds date

--- myApp/www/app/user_admin/ua.controllers.js
+++ myApp/www/app/user_admin/ua.controllers.js
@@ -137,6 +137,7 @@
     var ipObj1 = {
       callback: function (val) {  //Mandatory 
         console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+        vm.xDays.startDate = new Date(val);
       }
     }
     vm.openDatePicker = function(){
