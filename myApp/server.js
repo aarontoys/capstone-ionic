fe97f2c9235af9eac449474d0c134f7e0ea0bd35
Aaron Toys <aarontoys@gmail.com>
2017-02-02 20:09:29 -0700

    add server.js to create web server to render web-view and web-view.html for displaying the web-view

---
+++ myApp/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path')
+var app = express();
+var router = express.Router();
+
+
+app.use(express.static(path.resolve(__dirname, "www")));
+app.set('port', process.env.PORT || 3000);
+
+app.get('/web-view', function (req, res, next) {
+  console.log('web-view route hit');
+  res.sendFile(path.join(__dirname, 'www/web-view.html'));
+  // res.send('Hello World!')
+});
+
+app.listen(app.get('port'), function() {
+ console.log("listening to Port", app.get("port"));
+});
